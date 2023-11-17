0975e5482c2a81c5be690aa279297651fc7312ab
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-17 17:23:52 +0100

    Add Tailwind CSS configuration and styles

---
+++ testai/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, ts}"],
+  theme: {
+    fontFamily: {
+      sans: ["Helvetica", "Montserrat", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
